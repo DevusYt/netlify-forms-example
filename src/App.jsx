52327fdeb992a51f24dca2fb225bde2d53c7c537
Devus <schnellfeuerxd@gmail.com>
2023-03-20 20:03:49 +0100

    second test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ import React from 'react'
 
 function App() {
   return (
+
+
     <div>
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+
       <form method="post">
         <input type="hidden" name="form-name" value="contact" />
         <p>
