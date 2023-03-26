4dadc4b38fc038368a51d9de9d11c8675009a123
SchnellfeuerXD <schnellfeuerxd@gmail.com>
2023-03-26 18:01:11 +0200

    nav opt

--- src/MyNav.jsx
+++ src/MyNav.jsx
@@ -8,7 +8,7 @@ export default function MyNav() {
     return (
         <Navbar bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Forms-Test</Navbar.Brand>
+                <Navbar.Brand href="tryout">Forms-Test</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Static Forms</Nav.Link>
                     <Nav.Link href="stateful">Stateful Forms</Nav.Link>
