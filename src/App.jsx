2ef662ca15d495fda11c6399a28c250242fd554c
SchnellfeuerXD <schnellfeuerxd@gmail.com>
2023-03-26 14:59:13 +0200

    hope it still works

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,28 @@ function App() {
 
 
     <div>
-      <form name="Netlify Example Forms" netlify>
+
+      <MyNav />
+
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+
+      <form name="contact" method="post" class='d-flex'>
+        <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
+          <label>Your Name: <input type="text" name="name" /></label>
         </p>
         <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
+          <label>Your Email: <input type="email" name="email" /></label>
         </p>
         <p>
-          <Button variant="outline-primary" type="submit">Primary</Button>{' '}
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <Button variant="outline-primary" type="submit">Submit</Button>{' '}
         </p>
       </form>
     </div>
